b83bb8196782b4b2c577963e28caebcb82976674
SongBeom00 <kjb001023@naver.com>
2025-04-03 19:36:52 +0900

    jpa-basic-start

---
+++ src/main/java/jpabasic/ex1hellojpa/hellojpa/JpaMain.java
@@ -0,0 +1,41 @@
+package jpabasic.ex1hellojpa.hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+
+            //영속
+            Member member = new Member(200L,"member200");
+            em.persist(member);
+
+            em.flush();
+
+            System.out.println("=================");
+
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+
+        emf.close();
+
+
+    }
+}
